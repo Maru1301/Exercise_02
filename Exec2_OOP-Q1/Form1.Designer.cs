035f8d7c6b633868499d2ddb6e440023d08a35bf
Maru1301 <how7633818@gmail.com>
2022-10-29 15:26:35 +0800

    Exec2_OOP

---
+++ Exec2_OOP-Q1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Exec2_OOP_Q1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnGuess = new System.Windows.Forms.Button();
+			this.txtGuess = new System.Windows.Forms.TextBox();
+			this.btnAgain = new System.Windows.Forms.Button();
+			this.txtShow = new System.Windows.Forms.TextBox();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnGuess
+			// 
+			this.btnGuess.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btnGuess.Location = new System.Drawing.Point(233, 61);
+			this.btnGuess.Name = "btnGuess";
+			this.btnGuess.Size = new System.Drawing.Size(88, 35);
+			this.btnGuess.TabIndex = 1;
+			this.btnGuess.Text = "Enter";
+			this.btnGuess.UseVisualStyleBackColor = true;
+			this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+			// 
+			// txtGuess
+			// 
+			this.txtGuess.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.txtGuess.Location = new System.Drawing.Point(83, 61);
+			this.txtGuess.Name = "txtGuess";
+			this.txtGuess.Size = new System.Drawing.Size(129, 38);
+			this.txtGuess.TabIndex = 0;
+			// 
+			// btnAgain
+			// 
+			this.btnAgain.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btnAgain.Location = new System.Drawing.Point(126, 396);
+			this.btnAgain.Name = "btnAgain";
+			this.btnAgain.Size = new System.Drawing.Size(153, 36);
+			this.btnAgain.TabIndex = 2;
+			this.btnAgain.Text = "NewGame";
+			this.btnAgain.UseVisualStyleBackColor = true;
+			this.btnAgain.Click += new System.EventHandler(this.btnAgain_Click);
+			// 
+			// txtShow
+			// 
+			this.txtShow.Location = new System.Drawing.Point(83, 127);
+			this.txtShow.Multiline = true;
+			this.txtShow.Name = "txtShow";
+			this.txtShow.ReadOnly = true;
+			this.txtShow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtShow.Size = new System.Drawing.Size(254, 248);
+			this.txtShow.TabIndex = 4;
+			// 
+			// btnExit
+			// 
+			this.btnExit.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btnExit.Location = new System.Drawing.Point(316, 396);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(93, 36);
+			this.btnExit.TabIndex = 3;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.btnGuess;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(421, 444);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.txtShow);
+			this.Controls.Add(this.btnAgain);
+			this.Controls.Add(this.txtGuess);
+			this.Controls.Add(this.btnGuess);
+			this.Name = "Form1";
+			this.Text = "1A2B";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnGuess;
+		private System.Windows.Forms.TextBox txtGuess;
+		private System.Windows.Forms.Button btnAgain;
+		private System.Windows.Forms.TextBox txtShow;
+		private System.Windows.Forms.Button btnExit;
+	}
+}
+
---
+++ Exec2_OOP-Q1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_OOP_Q1
+{
+	public partial class Form1 : Form
+	{
+		private Game1A2B game = new Game1A2B();
+		public Form1()
+		{
+			InitializeComponent();
+
+			InitializeGame();
+
+		}
+
+		private void btnAgain_Click(object sender, EventArgs e)
+		{
+			InitializeGame();
+		}
+
+		private void InitializeGame()
+		{
+			game.GameStart();
+			txtGuess.Text = String.Empty;
+			txtShow.Text = String.Empty;
+		}
+
+		private void btnGuess_Click(object sender, EventArgs e)
+		{
+			string guess = GetInput();
+			if (!isInputValid(guess))
+			{
+				return;
+			}
+
+			ShowResult(guess);
+		}
+
+		private void ShowResult(string guess)
+		{
+			GameResult result = game.Check(guess);
+			if (result.isSuccess)
+			{
+				//todo success lblshow
+				txtShow.Text = result.Hint;
+				MessageBox.Show("Bingo!!!");
+
+				DialogResult dr =  MessageBox.Show("Do you wanna paly again?", "One more?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+				if(dr == DialogResult.Yes)
+				{
+					InitializeGame();
+				}
+				else
+				{
+					Application.Exit();
+				}
+			}
+			else
+			{
+				//todo fail lblshow
+				txtShow.Text = result.Hint;
+			}
+		}
+
+		private bool isInputValid(string guess)
+		{
+			if(guess.Length != 4)
+			{
+				MessageBox.Show("Invalid Input Length!\nYou need to input 4 numbers!");
+				return false;
+			}
+			bool isInt = int.TryParse(guess, out int value);
+
+			if (!isInt)
+			{
+				MessageBox.Show("Input Error!\nYou need to input 4 numbers!");
+				return false;
+			}
+
+			return true;
+		}
+
+		private string GetInput()
+		{
+			string input = txtGuess.Text;
+			return input;
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
+
+public class GameResult
+{
+	public bool isSuccess { get; set; }
+	public bool isFailed 
+	{
+		get { return !isSuccess; }
+	}
+	public string Hint { get; set; }
+}
+
+/// <summary>
+/// 1A2B Game
+/// </summary>
+public class Game1A2B
+{
+	private string answer;//store the answer
+
+	public string Hint 
+	{ 
+		get; 
+		set; 
+	}
+	/// <summary>
+	/// initialize the game
+	/// </summary>
+	public void GameStart()
+	{
+		answer = String.Empty;
+		this.Hint = String.Empty;
+		Random random = new Random();
+		int[] nums = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		for(int i = 0; i < nums.Length; i++)
+		{
+			int index = random.Next(0,10);
+			swap(ref nums[i], ref nums[index]);
+		}
+
+		for(int i = 0; i < 4; i++)
+		{
+			answer += nums[i].ToString();
+		}
+		//MessageBox.Show(answer);
+	}
+
+	public GameResult Check(string guess)
+	{
+		if(guess == answer)
+		{
+			this.Hint += $"[{answer}\t4A0B]";
+			return new GameResult { isSuccess = true, Hint = this.Hint };
+		}
+
+		int A = 0, B = 0;
+		for(int i = 0; i < guess.Length; i++)
+		{
+			for(int j = 0; j < answer.Length; j++)
+			{
+				if (guess[i] == answer[j] && i == j)
+				{
+					A++;
+				}
+				else if( guess[i] == answer[j])
+				{
+					B++;
+				}
+			}
+		}
+
+		this.Hint += $"{guess}\t{A}A{B}B\r\n";
+		return new GameResult { isSuccess = false, Hint = this.Hint };
+	}
+
+	/// <summary>
+	/// interchange the values of two arguments
+	/// </summary>
+	/// <param name="v1">argument 1</param>
+	/// <param name="v2">argument 2</param>
+	private void swap(ref int v1, ref int v2)
+	{
+		int temp = v1;
+		v1 = v2;
+		v2 = temp;
+	}
+}
